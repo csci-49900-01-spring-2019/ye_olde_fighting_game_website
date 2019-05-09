56a2baf77e2f3dc46dedfceefea5bab72f8d752e
Truly <trulyj.sof@gmail.com>
2019-05-09 09:30:16 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_08_185121) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stats", force: :cascade do |t|
     t.text "username"
     t.float "avg_rank"
